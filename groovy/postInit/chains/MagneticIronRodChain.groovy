9c3a4d3badae4223e010bfcfe8574576391bc9e6
Loxo <loxo.Minecraft@gmail.com>
2023-03-04 14:30:08 +0100

    Add alternative recipe for Electrolyte Paper, make Voltaic Pile reusable

--- groovy/postInit/chains/MagneticIronRodChain.groovy
+++ groovy/postInit/chains/MagneticIronRodChain.groovy
@@ -1,10 +1,5 @@
 import static globals.Globals.*
 
-def electrolyte_paper_ingredients = [
-    item('forge:bucketfilled').withNbt([FluidName: "salt_water", Amount: 1000]),
-    item('minecraft:paper')
-]
-
 def voltaic_pile_ingredients = [
     [null, metaitem('electrolyte_paper'), null],
     [null, metaitem('plateZinc'), null],
@@ -12,16 +7,36 @@ def voltaic_pile_ingredients = [
 ]
 
 def magnetic_rod_ingredients = [
-    [metaitem('voltaic_pile'),  metaitem('wireGtSingleCopper'), metaitem('springCopper')],
+    [metaitem('voltaic_pile').reuse(), metaitem('wireGtSingleCopper').reuse(), metaitem('springCopper').reuse()],
     [null, null, metaitem('hot_iron_rod')],
     [null, null, null]
 ]
 
+crafting.removeByOutput(metaitem('stickIronMagnetic')) 
+
 furnace.add(metaitem('stickIron'), metaitem('hot_iron_rod'))
 
-crafting.addShapeless(metaitem('electrolyte_paper'), electrolyte_paper_ingredients) 
-crafting.addShaped(metaitem('voltaic_pile'), voltaic_pile_ingredients) 
-
-crafting.removeByOutput(metaitem('stickIronMagnetic')) 
-crafting.addShapeless(metaitem('stickIronMagnetic') * 2, [metaitem('hot_iron_rod'), metaitem('stickIronMagnetic')])
+crafting.addShapeless(metaitem('electrolyte_paper'), [
+    item('forge:bucketfilled').withNbt([FluidName: "salt_water", Amount: 1000]),
+    item('minecraft:paper')
+]) 
+crafting.addShapeless(metaitem('electrolyte_paper'), [
+    metaitem('food.lime'),
+    item('minecraft:paper'),
+    ore('craftingToolMortar')
+]) 
+crafting.addShapeless(metaitem('electrolyte_paper'), [
+    metaitem('food.lemon'),
+    item('minecraft:paper'),
+    ore('craftingToolMortar')
+]) 
+crafting.addShapeless(metaitem('stickIronMagnetic') * 2, [
+    metaitem('hot_iron_rod'), 
+    metaitem('stickIronMagnetic')
+])
+crafting.addShapeless(metaitem('stickIronMagnetic'), [
+    metaitem('hot_iron_rod'), 
+    metaitem('chunk.magnetite')
+])
 crafting.addShaped(metaitem('stickIronMagnetic'), magnetic_rod_ingredients)
+crafting.addShaped(metaitem('voltaic_pile'), voltaic_pile_ingredients)
