2d286d8143cf969bafd1f35d46117e60138dc1e2
htmlcsjs <46023024+htmlcsjs@users.noreply.github.com>
2023-04-10 19:54:50 +0100

    Update launch.sh to fix issues starting server

--- launch.sh
+++ launch.sh
@@ -3,7 +3,7 @@
 # If you are having problems, make sure your java version is 1.8.0_xx with java -version
 
 RAM='2048M'
-FORGEJAR="forge-1.12.2-14.23.5.2855.jar"
+FORGEJAR="forge-1.12.2-14.23.5.2860.jar"
 
 PARAMS="-server -Xms$RAM -Xmx$RAM -jar $FORGEJAR nogui"
 
