9b1829c0b362911f4493ca9de223c544c41ff0d0
Zalgo239 <67443118+Zalgo239@users.noreply.github.com>
2023-04-11 21:09:48 +0200

    LittleTiles gregification begin

---
+++ groovy/postInit/mod/LittleTiles.groovy
@@ -0,0 +1,5 @@
+crafting.replaceShaped('littletiles:hammer', item('littletiles:hammer'), [
+    [ore('plateIron'), ore('plateIron'), ore('plateIron')],
+    [ore('dyeBlue'), ore('stickIron'), null],
+    [ore('dyeBlue'), ore('stickIron'), null]
+])
