dc909fb3d5ae4e32cfeae53f92b97fe2daa215a0
BestMod <71246119+BestMod@users.noreply.github.com>
2023-04-18 17:51:02 -0700

    Update Batteries.groovy

--- groovy/postInit/progression/Batteries.groovy
+++ groovy/postInit/progression/Batteries.groovy
@@ -2,6 +2,8 @@ import static globals.Globals.*
 
 import gregtech.api.recipes.recipeproperties.RecipeProperty;
 import gregtech.api.recipes.RecipeBuilder;
+import static gregtech.api.recipes.RecipeMaps.*;
+import gregtech.api.unification.material.Materials.*;
 
 import gregtech.api.recipes.ModHandler;
 import gregtech.api.unification.material.Materials;
@@ -25,3 +27,15 @@ crafting.addShaped("anode_lead", metaitem('anode.lead'), [
         [null,metaitem('plateLead'),metaitem('cableGtSingleTin')],
         [null,null,null]
 ]);
+crafting.addShaped("drum_lead", metaitem('drum.lead'), [
+        [null,null,null],
+        [metaitem('plateLead'),metaitem('stickLongLead'),metaitem('plateLead')],
+        [metaitem('plateLead'),metaitem('stickLongLead'),metaitem('plateLead')]
+]);
+MIXER_RECIPES.recipeBuilder()
+.fluidInputs(Materials.SulfurTrioxide.getFluid(1000))
+.fluidInputs(Materials.Water.getFluid(1000))
+.fluidOutputs(Materials.SulfuricAcid.getFluid(1000))
+.EUt(7)
+.duration(40)
+.buildAndRegister()
