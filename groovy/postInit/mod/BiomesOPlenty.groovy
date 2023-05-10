e3609c88027ae9eaf4ea2953c3cf31ee178da24f
Memeexp <76496033+Memeexp@users.noreply.github.com>
2023-05-09 21:21:49 -0700

    Added ceramic bucket mud recipe

---
+++ groovy/postInit/mod/BiomesOPlenty.groovy
@@ -0,0 +1,2 @@
+crafting.addShapeless("biomesoplenty:mud", 
+item('biomesoplenty:mud'), [ore('dirt'), item('notreepunching:ceramic_bucket').withNbt(["Fluid":["FluidName": "water", "Amount": 1000]])]);
