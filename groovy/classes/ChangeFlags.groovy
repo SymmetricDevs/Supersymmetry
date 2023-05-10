2fb78441fff0f3afa3efeb4ad980fd3292f80b0a
Regian <64612588+Regian24@users.noreply.github.com>
2023-05-10 20:09:32 +0200

    Add borax ore (#218)
    * Auto stash before checking out "HEAD"
    
    * add borax ore
    
    ---------
    
    Co-authored-by: mtbo <111296252+loxoDev@users.noreply.github.com>

--- groovy/classes/ChangeFlags.groovy
+++ groovy/classes/ChangeFlags.groovy
@@ -24,6 +24,11 @@ class ChangeFlags {
         Tantalum.addFlags("generate_rod");
 
         GregTechAPI.MaterialRegistry.get("fluix").addFlags("disable_decomposition", "generate_plate", "generate_lens")
+        //Iodine.dust().build();
+        //Hafnium.ingot().build();
+        //Strontium.ingot().build();
+      
+		    Borax.setProperty(PropertyKey.ORE, new OreProperty());
 
         ManganesePhosphide.addFlags("generate_fine_wire");
         UraniumTriplatinum.addFlags("generate_fine_wire");
