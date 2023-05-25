06fdd02796723add25f9f83dbbb4c14dd2f97f21
Regian <64612588+Regian24@users.noreply.github.com>
2023-05-25 14:40:23 +0200

    Update DecorationBlocks.groovy

--- groovy/postInit/mod/DecorationBlocks.groovy
+++ groovy/postInit/mod/DecorationBlocks.groovy
@@ -1,46 +1,46 @@
 
 
 crafting.addShaped('susy:gabbro_bricks', item('susy:susy_stone_bricks') * 4, [
-        [ore('stoneGabbro'), ore('stoneGabbro'),
-        [ore('stoneGabbro'), ore('stoneGabbro')
+        [ore('stoneGabbro'), ore('stoneGabbro')],
+        [ore('stoneGabbro'), ore('stoneGabbro')]
 ])
 
 crafting.addShaped('susy:gneiss_bricks', item('susy:susy_stone_bricks', 1) * 4, [
-        [ore('stoneGneiss'), ore('stoneGneiss'),
-        [ore('stoneGneiss'), ore('stoneGneiss')
+        [ore('stoneGneiss'), ore('stoneGneiss')],
+        [ore('stoneGneiss'), ore('stoneGneiss')]
 ])
 
 crafting.addShaped('susy:limestone_bricks', item('susy:susy_stone_bricks', 2) * 4, [
-        [ore('stoneLimestone'), ore('stoneLimestone'),
-        [ore('stoneLimestone'), ore('stoneLimestone')
+        [ore('stoneLimestone'), ore('stoneLimestone')],
+        [ore('stoneLimestone'), ore('stoneLimestone')]
 ])
 
 crafting.addShaped('susy:phyllite_bricks', item('susy:susy_stone_bricks', 3) * 4, [
-        [ore('stonePhyllite'), ore('stonePhyllite'),
-        [ore('stonePhyllite'), ore('stonePhyllite')
+        [ore('stonePhyllite'), ore('stonePhyllite')],
+        [ore('stonePhyllite'), ore('stonePhyllite')]
 ])
 
 crafting.addShaped('susy:quartzite_bricks', item('susy:susy_stone_bricks', 4) * 4, [
-        [ore('stoneQuartzite'), ore('stoneQuartzite'),
-        [ore('stoneQuartzite'), ore('stoneQuartzite')
+        [ore('stoneQuartzite'), ore('stoneQuartzite')],
+        [ore('stoneQuartzite'), ore('stoneQuartzite')]
 ])
 
 crafting.addShaped('susy:shale_bricks', item('susy:susy_stone_bricks', 5) * 4, [
-        [ore('stoneShale'), ore('stoneShale'),
-        [ore('stoneShale'), ore('stoneShale')
+        [ore('stoneShale'), ore('stoneShale')],
+        [ore('stoneShale'), ore('stoneShale')]
 ])
 
 crafting.addShaped('susy:slate_bricks', item('susy:susy_stone_bricks', 6) * 4, [
-        [ore('stoneSlate'), ore('stoneSlate'),
-        [ore('stoneSlate'), ore('stoneSlate')
+        [ore('stoneSlate'), ore('stoneSlate')],
+        [ore('stoneSlate'), ore('stoneSlate')]
 ])
 
 crafting.addShaped('susy:soapstone_bricks', item('susy:susy_stone_bricks', 7) * 4, [
-        [ore('stoneSoapstone'), ore('stoneSoapstone'),
-        [ore('stoneSoapstone'), ore('stoneSoapstone')
+        [ore('stoneSoapstone'), ore('stoneSoapstone')],
+        [ore('stoneSoapstone'), ore('stoneSoapstone')]
 ])
 
 crafting.addShaped('susy:kimberlite_bricks', item('susy:susy_stone_bricks', 8) * 4, [
-        [ore('stoneKimberlite'), ore('stoneKimberlite'),
-        [ore('stoneKimberlite'), ore('stoneKimberlite')
+        [ore('stoneKimberlite'), ore('stoneKimberlite')],
+        [ore('stoneKimberlite'), ore('stoneKimberlite')]
 ])
