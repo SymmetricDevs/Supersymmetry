21bd81bb8c68168db96c146199d8daeb200e63c8
igobylotsofnames <igblon@outlook.com>
2023-06-02 08:48:02 -0700

    fix stuff

--- groovy/material/ThirdDegreeMaterials.groovy
+++ groovy/material/ThirdDegreeMaterials.groovy
@@ -124,13 +124,13 @@ public class ThirdDegreeMaterials {
                 .colorAverage()
                 .build()
 
-      TwoButanolSolution = new Material.Builder(24017, 'two_butanol_solution')
+      TwoButanolSolution = new Material.Builder(24018, 'two_butanol_solution')
                 .fluid()
                 .components(SulfuricAcid, 1, TwoButanol, 1)
                 .colorAverage()
                 .build()
 
-        FourFourOxydianilineSolution = new Material.Builder(24018, 'four_four_oxydianiline_solution')
+        FourFourOxydianilineSolution = new Material.Builder(24019, 'four_four_oxydianiline_solution')
                 .fluid()
                 .components(Water, 5, FourFourOxydianiline, 1)
                 .colorAverage()
