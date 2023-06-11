9b9f3b200bc0dd313156eb628da41225032744f1
Memeexp <76496033+Memeexp@users.noreply.github.com>
2023-06-11 14:48:58 -0700

    Patched NTP knife recipe durability bug

--- groovy/postInit/chains/BrickChain.groovy
+++ groovy/postInit/chains/BrickChain.groovy
@@ -77,6 +77,14 @@ def mudbrick_mix_ingredients = [
 
 crafting.addShapeless('ManualMudbrickMix',metaitem('mudbrick_mix') * 2, mudbrick_mix_ingredients) 
 crafting.addShapeless('StrawCutting',metaitem('straw') * 2, [ore('craftingToolKnife'), item('notreepunching:grass_fiber')]) 
-crafting.addShapeless('StrawCuttingNTP',metaitem('straw') * 2, [ore('toolWeakKnife').reuse(), item('notreepunching:grass_fiber')]) 
+
+def damage = { 
+    stack -> 
+        stack.setItemDamage(stack.getItemDamage() + 1)
+        if (stack.getItemDamage() <= stack.getMaxDamage()) {
+            stack.copy()
+        }
+}
+crafting.addShapeless('StrawCuttingNTP',metaitem('straw') * 2, [ore('toolWeakKnife').transform(damage), item('notreepunching:grass_fiber')]) 
 
 CompressBrickRecipes('mudbrick_mix', 'brick.adobe')
