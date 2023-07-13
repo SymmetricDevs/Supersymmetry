152a0c7d65c72a64ef8d14fd2bb969a7a97ca153
Regian24 <64612588+Regian24@users.noreply.github.com>
2023-07-13 15:40:17 +0200

    remove drawer key recipe

--- groovy/postInit/mod/StorageDrawers.groovy
+++ groovy/postInit/mod/StorageDrawers.groovy
@@ -6,7 +6,16 @@ def name_removals = [
     'storagedrawers:upgrade_storage_diamond',
     'storagedrawers:upgrade_storage_emerald',
     'storagedrawers:upgrade_void',
-    'storagedrawers:upgrade_conversion'
+    'storagedrawers:upgrade_conversion',
+    'storagedrawers:key_drawer',
+	'storagedrawers:key_concealment',
+	'storagedrawers:key_personal',
+    'storagedrawers:key_quantify',
+	'storagedrawers:keybutton_drawer',
+    'storagedrawers:keybutton_concealment',
+    'storagedrawers:keybutton_personal',
+    'storagedrawers:keybutton_quantify'
+
 ]
 
 for (name in name_removals) {
