ef1bac1799a7a04de3a618b67d4c7756e63af486
planetme <87911459+planetme@users.noreply.github.com>
2023-07-24 04:18:10 -0500

    0.1.6 chem (#470)
    * Remove small dusts, rebalance electrolysis
    
    * Added spool to mineral wool spinning
    
    * Debuff gasoline durations
    
    While I upgraded the energy produced, I forgot to lower the duration to an appropriate time :skull:
    
    * Prevent recipe overlaps
    
    * Buff coal for power generation
    
    * manganese fix part deux
    
    * D2O fast electrolysis
    
    * Bugfixes

--- groovy/postInit/chains/DeuteriumChain.groovy
+++ groovy/postInit/chains/DeuteriumChain.groovy
@@ -3,6 +3,7 @@ import static globals.Globals.*
 
 def LCR = recipemap('large_chemical_reactor');
 def ELECTROLYZER = recipemap('electrolyzer');
+def ELECTROLYTIC_CELL = recipemap('electrolytic_cell');
 
 LCR.recipeBuilder().EUt(480).duration(200)
 	.fluidInputs(fluid('hydrogen_sulfide') * 2000)
@@ -69,3 +70,14 @@ ELECTROLYZER.recipeBuilder()
 		.EUt(30)
 		.duration(1500)
 		.buildAndRegister()
+
+ELECTROLYTIC_CELL.recipeBuilder()
+		.notConsumable(metaitem('stickNickel'))
+		.notConsumable(metaitem('stickIron'))
+		.notConsumable(fluid('sodium_hydroxide_solution') * 50)
+		.fluidInputs(fluid('heavy_water') * 1000)
+		.fluidOutputs(fluid('deuterium') * 2000)
+		.fluidOutputs(fluid('oxygen') * 1000)
+		.EUt(30)
+        .duration(200)
+        .buildAndRegister()
