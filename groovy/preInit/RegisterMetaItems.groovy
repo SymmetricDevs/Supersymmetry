3e481f40baf11867d1115cf73d102337ba20c5ab
mtbo <111296252+loxoDev@users.noreply.github.com>
2023-09-02 14:14:08 +0200

    Update RegisterMetaItems.groovy

--- groovy/preInit/RegisterMetaItems.groovy
+++ groovy/preInit/RegisterMetaItems.groovy
@@ -121,8 +121,7 @@ eventManager.listen {
         customMetaItems.addItem(208, "sponge.hafnium.crude")
         customMetaItems.addItem(209, "sponge.platinum")
 
-		    customMetaItems.addItem(210, "bound_leather")
-        customMetaItems.addItem(211, "copper_anode_slime")
+	customMetaItems.addItem(210, "bound_leather")
 
         customMetaItems.addItem(212, "drone.lv").setMaxStackSize(1).addComponents(ElectricStats.createRechargeableBattery(10000, GTValues.LV)).setModelAmount(8).setCreativeTabs(GregTechAPI.TAB_GREGTECH_TOOLS);
 
