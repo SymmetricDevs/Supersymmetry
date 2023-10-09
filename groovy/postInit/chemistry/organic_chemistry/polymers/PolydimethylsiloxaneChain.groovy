b33b8ad9ef0766d3506cfbf1d1c07b068ac0501e
05council1 <128324236+05council1@users.noreply.github.com>
2023-10-08 21:20:12 -0400

    wip edit

--- groovy/postInit/chemistry/organic_chemistry/polymers/PolydimethylsiloxaneChain.groovy
+++ groovy/postInit/chemistry/organic_chemistry/polymers/PolydimethylsiloxaneChain.groovy
@@ -1,1 +1,10 @@
+import static globals.Globals.*
 
+
+DISTILLATION_TOWER = recipemap('distillation_tower')
+FLUIDIZED_BED_REACTOR = recipemap('fluidized_bed_reactor')
+MIXER = recipemap('mixer')
+CENTRIFUGE = recipemap('centrifuge')
+ROASTER = recipemap('roaster')
+
+FLUIDIZED_BED_REACTOR.recipeBuilder()
