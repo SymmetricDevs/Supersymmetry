efcbb243e9f22f2a5c31217573133f6261deec67
trainvoi <voitrainelephant@gmail.com>
2023-12-16 21:01:55 -0500

    add recipe to turn liquid resin into sticky resin item with fluid solidifier

--- groovy/postInit/biology/ResinChain.groovy
+++ groovy/postInit/biology/ResinChain.groovy
@@ -15,12 +15,27 @@ crafting.addShaped('gregtech:resin_item', metaitem('rubber_drop') * 4, [
 
 def DISTILLERY = recipemap('distillery');
 def CENTRIFUGE = recipemap('centrifuge');
+def FLUID_SOLIDIFIER = recipemap('fluid_solidifier');
 
 DISTILLERY.recipeBuilder()
         .fluidInputs(fluid('resin') * 100)
         .fluidOutputs(fluid('glue') * 75)
         .duration(15)
         .EUt(30)
+        .buildAndRegister()
+
+FLUID_SOLIDIFIER.recipeBuilder()
+        .fluidInputs(fluid('resin') * 250)
+        .outputs(metaitem('rubber_drop'))
+        .duration(20)
+        .EUt(2)
+        .buildAndRegister()
+
+CENTRIFUGE.recipeBuilder()
+        .inputs(metaitem('rubber_drop'))
+        .fluidOutputs(fluid('resin') * 250)
+        .duration(40)
+        .EUt(8)
         .buildAndRegister()
 
 CENTRIFUGE.recipeBuilder()
