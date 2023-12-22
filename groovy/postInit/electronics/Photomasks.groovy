830f72569ae31520887e884583fb11f3ad4f3480
planetme <87911459+planetme@users.noreply.github.com>
2023-12-21 19:15:05 -0600

    finish circuit rework part I

--- groovy/postInit/electronics/Photomasks.groovy
+++ groovy/postInit/electronics/Photomasks.groovy
@@ -3,6 +3,9 @@ import static globals.Globals.*
 ASSEMBLER = recipemap('assembler')
 BR = recipemap('batch_reactor')
 REACTION_FURNACE = recipemap('reaction_furnace')
+ROASTER = recipemap('roaster')
+MIXER = recipemap('mixer')
+UV_LIGHT_BOX = recipemap('uv_light_box')
 
 // TMPTA
 
@@ -60,8 +63,9 @@ BR.recipeBuilder()
 
 MIXER.recipeBuilder()
     .inputs(ore('dustBenzenediazoniumChloride'))
+    .inputs(ore('dustGelatin') * 8)
     .fluidInputs(fluid('trimethylolpropane_triacrylate') * 8000)
-    .fluidOutputs(fluid('photoemulsion') * 8000)
+    .fluidOutputs(fluid('acrylic_photoemulsion') * 8000)
     .duration(200)
     .EUt(120)
     .buildAndRegister()
@@ -69,7 +73,7 @@ MIXER.recipeBuilder()
 MIXER.recipeBuilder()
     .inputs(ore('foilPlastic'))
     .fluidInputs(fluid('dye_red') * 100)
-    .fluidInputs(fluid('photoemulsion') * 500)
+    .fluidInputs(fluid('acrylic_photoemulsion') * 500)
     .outputs(metaitem('rubylith_film'))
     .duration(200)
     .EUt(120)
@@ -80,6 +84,148 @@ ASSEMBLER.recipeBuilder()
     .inputs(metaitem('rubylith_film'))
     .fluidInputs(fluid('glue') * 100)
     .outputs(metaitem('rubylith'))
+    .duration(200)
+    .EUt(120)
+    .buildAndRegister()
+
+// Early lithography masks
+
+ROASTER.recipeBuilder()
+    .inputs(ore('dustSilver'))
+    .fluidInputs(fluid('chlorine') * 1000)
+    .outputs(metaitem('dustSilverChloride') * 2)
+    .duration(100)
+    .EUt(30)
+    .buildAndRegister()
+
+ASSEMBLER.recipeBuilder()
+    .inputs(ore('plateGlass'))
+    .inputs(ore('dustGelatin'))
+    .inputs(ore('dustSilverChloride'))
+    .outputs(metaitem('mask.blank'))
+    .duration(200)
+    .EUt(7)
+    .buildAndRegister()
+
+crafting.addShaped("rubylith_ram", metaitem('stencil.ram'), [
+    [ore('craftingToolKnife'), null, null],
+    [null, metaitem('rubylith'), null],
+    [null, null, null]
+]);
+
+UV_LIGHT_BOX.recipeBuilder()
+    .inputs(metaitem('stencil.ram'))
+    .inputs(metaitem('mask.blank'))
+    .outputs(metaitem('mask.ram'))
+    .duration(200)
+    .EUt(7)
+    .buildAndRegister()
+
+crafting.addShaped("rubylith_ic", metaitem('stencil.ic'), [
+    [null, ore('craftingToolKnife'), null],
+    [null, metaitem('rubylith'), null],
+    [null, null, null]
+]);
+
+UV_LIGHT_BOX.recipeBuilder()
+    .inputs(metaitem('stencil.ic'))
+    .inputs(metaitem('mask.blank'))
+    .outputs(metaitem('mask.ic'))
+    .duration(200)
+    .EUt(7)
+    .buildAndRegister()
+
+crafting.addShaped("rubylith_pcb", metaitem('stencil.pcb'), [
+    [null, null, ore('craftingToolKnife')],
+    [null, metaitem('rubylith'), null],
+    [null, null, null]
+]);
+
+UV_LIGHT_BOX.recipeBuilder()
+    .inputs(metaitem('stencil.pcb'))
+    .inputs(metaitem('mask.blank'))
+    .outputs(metaitem('mask.pcb'))
+    .duration(200)
+    .EUt(7)
+    .buildAndRegister()
+
+crafting.addShaped("rubylith_ulpic", metaitem('stencil.ulpic'), [
+    [null, null, null],
+    [ore('craftingToolKnife'), metaitem('rubylith'), null],
+    [null, null, null]
+]);
+
+UV_LIGHT_BOX.recipeBuilder()
+    .inputs(metaitem('stencil.ulpic'))
+    .inputs(metaitem('mask.blank'))
+    .outputs(metaitem('mask.ulpic'))
+    .duration(200)
+    .EUt(7)
+    .buildAndRegister()
+
+crafting.addShaped("rubylith_lpic", metaitem('stencil.lpic'), [
+    [null, null, null],
+    [null, metaitem('rubylith'), ore('craftingToolKnife')],
+    [null, null, null]
+]);
+
+UV_LIGHT_BOX.recipeBuilder()
+    .inputs(metaitem('stencil.lpic'))
+    .inputs(metaitem('mask.blank'))
+    .outputs(metaitem('mask.lpic'))
+    .duration(200)
+    .EUt(7)
+    .buildAndRegister()
+
+crafting.addShaped("rubylith_cpu", metaitem('stencil.cpu'), [
+    [null, null, null],
+    [null, metaitem('rubylith'), null],
+    [ore('craftingToolKnife'), null, null]
+]);
+
+UV_LIGHT_BOX.recipeBuilder()
+    .inputs(metaitem('stencil.cpu'))
+    .inputs(metaitem('mask.blank'))
+    .outputs(metaitem('mask.cpu'))
+    .duration(200)
+    .EUt(7)
+    .buildAndRegister()
+
+// CrO3 photomasks
+
+REACTION_FURNACE.recipeBuilder()
+    .notConsumable(metaitem('shape.mold.plate'))
+    .fluidInputs(fluid('silicon_tetrachloride') * 1000)
+    .fluidInputs(fluid('oxygen') * 3000)
+    .fluidInputs(fluid('hydrogen') * 2000)
+    .outputs(metaitem('fused_quartz'))
+    .duration(400)
+    .EUt(120)
+    .buildAndRegister()
+
+ASSEMBLER.recipeBuilder()
+    .inputs(metaitem('fused_quartz'))
+    .inputs(ore('dustTinyChromiumTrioxide'))
+    .circuitMeta(1)
+    .outputs(metaitem('mask.advanced'))
+    .duration(200)
+    .EUt(120)
+    .buildAndRegister()
+
+ASSEMBLER.recipeBuilder()
+    .inputs(metaitem('fused_quartz'))
+    .inputs(ore('dustTinyChromiumTrioxide'))
+    .circuitMeta(2)
+    .outputs(metaitem('mask.nand'))
+    .duration(200)
+    .EUt(120)
+    .buildAndRegister()
+
+ASSEMBLER.recipeBuilder()
+    .inputs(metaitem('fused_quartz'))
+    .inputs(ore('dustTinyChromiumTrioxide'))
+    .circuitMeta(3)
+    .outputs(metaitem('mask.nor'))
     .duration(200)
     .EUt(120)
     .buildAndRegister()
--- groovy/preInit/RegisterMetaItems.groovy
+++ groovy/preInit/RegisterMetaItems.groovy
@@ -214,17 +214,25 @@ eventManager.listen {
         customMetaItems.addItem(4101, "vacuum_tube_components");
 
         //Circuit Stuff 5000-6000
-
-        customMetaItems.addItem(5000, "mask.ic");
-        customMetaItems.addItem(5001, "mask.cpu");
-        customMetaItems.addItem(5002, "mask.ram");
-        customMetaItems.addItem(5003, "mask.pcb");
-        customMetaItems.addItem(5004, "mask.ulpic");
-        customMetaItems.addItem(5005, "mask.lpic");
-        customMetaItems.addItem(5006, "mask.nand");
-        customMetaItems.addItem(5007, "mask.nor");
-        customMetaItems.addItem(5008, "mask.advanced");
-
+        //Photomasks 5000-5099
+        customMetaItems.addItem(5000, "rubylith")
+        customMetaItems.addItem(5001, "rubylith_film")
+        customMetaItems.addItem(5002, "stencil.ic")
+        customMetaItems.addItem(5003, "stencil.cpu")
+        customMetaItems.addItem(5004, "stencil.ram")
+        customMetaItems.addItem(5005, "stencil.pcb")
+        customMetaItems.addItem(5006, "stencil.ulpic")
+        customMetaItems.addItem(5007, "stencil.lpic")
+        customMetaItems.addItem(5020, "mask.blank")
+        customMetaItems.addItem(5021, "mask.ic");
+        customMetaItems.addItem(5022, "mask.cpu");
+        customMetaItems.addItem(5023, "mask.ram");
+        customMetaItems.addItem(5024, "mask.pcb");
+        customMetaItems.addItem(5025, "mask.ulpic");
+        customMetaItems.addItem(5026, "mask.lpic");
+        customMetaItems.addItem(5027, "mask.nand");
+        customMetaItems.addItem(5028, "mask.nor");
+        customMetaItems.addItem(5029, "mask.advanced");
         customMetaItems.addItem(5100, "patterned.ic");
         customMetaItems.addItem(5101, "patterned.cpu");
         customMetaItems.addItem(5102, "patterned.ram");
@@ -237,9 +245,7 @@ eventManager.listen {
         customMetaItems.addItem(5109, "patterned.nor");
         customMetaItems.addItem(5150, "patterned.board.phenolic");
         customMetaItems.addItem(5151, "patterned.board.plastic");
-
         customMetaItems.addItem(5200, "laminated.board.phenolic");
-
         customMetaItems.addItem(5300, "etched.ic");
         customMetaItems.addItem(5301, "etched.cpu");
         customMetaItems.addItem(5302, "etched.ram");
@@ -248,11 +254,6 @@ eventManager.listen {
         customMetaItems.addItem(5305, "etched.silicon_dioxide");
         customMetaItems.addItem(5306, "etched.polysilicon");
         customMetaItems.addItem(5307, "etched.silicon_nitride");
-
-                // Photomasks: 5900-6000
-
-                customMetaItems.addItem(5900, "rubylith")
-                custonMetaImtes.addItem(5901, "rubylith_film")
 
         //Light Sources 6000-6100
         customMetaItems.addItem(6000, "carbon_arc_lamp");
