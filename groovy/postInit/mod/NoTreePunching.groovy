82fe75fc460a1487c93fb0516b5113cb70eba837
trainvoi <voitrainelephant@gmail.com>
2024-02-03 20:25:52 -0500

    remove duplicate NTP recipe

--- groovy/postInit/mod/NoTreePunching.groovy
+++ groovy/postInit/mod/NoTreePunching.groovy
@@ -2,4 +2,6 @@ for(int i = 1; i < 35; i++){
     crafting.remove("notreepunching:saw_planks_" + i.toString());
 }
 
+    crafting.remove("notreepunching:saw_log_sticks");
+
 furnace.removeByInput(item('notreepunching:pottery/small_vessel'))
