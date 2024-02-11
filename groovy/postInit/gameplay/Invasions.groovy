a97a6737385883840b45cb14465d059bac5fa2e9
bruberu <80226372+bruberu@users.noreply.github.com>
2024-02-11 15:10:47 -0600

    Invasions (#735)
    * fix some groovy bugs
    
    * fix diluted ammonia distillation DT
    
    * fixed everything
    
    * me when uneven amt of brackets
    
    * planet you forgot to update susymaterials
    
    * Update ChemistryOverhaul.groovy
    
    * add Adv. Arc Furnace to Chem Overhaul recipe map
    
    * fix duplicate aqua regia recipe removal
    
    * turrets no longer require a freshly crafted pistol
    
    * Add Invasions.groovy
    
    * Yeet onslaught
    
    ---------
    
    Co-authored-by: trainvoi <voitrainelephant@gmail.com>
    Co-authored-by: planetme <87911459+planetme@users.noreply.github.com>

---
+++ groovy/postInit/gameplay/Invasions.groovy
@@ -0,0 +1,12 @@
+import supersymmetry.api.event.MobHordeEvent;
+import net.minecraft.entity.monster.EntityZombie;
+import techguns.entities.npcs.Bandit;
+
+new MobHordeEvent((player) -> {return new EntityZombie(player.world);}, 5, 10)
+	.setNightOnly(true)
+	.setTimer(10000, 20000)
+	
+new MobHordeEvent((player) -> {Bandit bandit = new Bandit(player.world); bandit.addRandomArmor(0); return bandit;}, 2, 4)
+	.setAdvancementUnlock(new ResourceLocation("gregtech:low_voltage/23_lv_assembler"))
+	.setMaximumDistanceUnderground(20)
+	.setTimer(6000, 15000)
