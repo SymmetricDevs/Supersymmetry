2ed58d5268d2c841885040115cc18d7f047f375f
Loxo <loxo.Minecraft@gmail.com>
2024-02-12 10:57:28 +0100

    Balance amount of invasions

--- groovy/postInit/gameplay/Invasions.groovy
+++ groovy/postInit/gameplay/Invasions.groovy
@@ -4,8 +4,8 @@ import techguns.entities.npcs.Bandit;
 
 new MobHordeEvent((player) -> {return new EntityZombie(player.world);}, 5, 10)
 	.setNightOnly(true)
-	.setTimer(10000, 20000)
+	.setTimer(216000, 360000)		// 3 - 5 hours
 	
 new MobHordeEvent((player) -> {Bandit bandit = new Bandit(player.world); bandit.addRandomArmor(0); return bandit;}, 2, 4)
 	.setAdvancementUnlock(new ResourceLocation("gregtech:low_voltage/23_lv_assembler"))
-	.setTimer(6000, 15000)
+	.setTimer(360000, 720000)			// 5-10 hours
