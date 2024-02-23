bf870e36e623f71926fe61f0ab6e323c5339e3f2
bruberu <80226372+bruberu@users.noreply.github.com>
2024-02-22 20:24:55 -0600

    fix: use steel boilers as the advancement instead

--- groovy/postInit/gameplay/Invasions.groovy
+++ groovy/postInit/gameplay/Invasions.groovy
@@ -13,7 +13,7 @@ new MobHordeEvent((player) -> {EntityZombie zombie = new EntityZombie(player.wor
 			        zombie.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, 999999, 1));
 			        zombie.addPotionEffect(new PotionEffect(MobEffects.SPEED, 999999));
 				return zombie;}, 6, 12, "zombie_hard")
-	.setAdvancementUnlock(new ResourceLocation("gregtech:steam/4_bronze_boiler"))
+	.setAdvancementUnlock(new ResourceLocation("gregtech:steam/16_steel_boiler"))
 	.setNightOnly(true)
 	.setTimer(144000, 216000)		// 2 - 3 hours
 
