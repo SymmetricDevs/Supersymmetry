8bc61f34cd69b34d3b24a7659b414e3cc6ad01b0
TheScienceDemon <louisruppert0@gmail.com>
2024-03-07 22:28:06 +0100

    Make questbook.py deactivate edit mode

--- build/questbook.py
+++ build/questbook.py
@@ -80,7 +80,7 @@ uselessProps = {
     "bg_size:3": 256,
 
     # delete edit mode if its true
-    "editmode:1": 1,
+    #"editmode:1": 1, # Actually don't because edit mode is enabled by default
 }
 
 basePath = os.path.normpath(os.path.abspath(__file__ + "/../../"))
@@ -99,6 +99,9 @@ def nest(location: dict) -> dict:
             del location[entry]
         elif (isinstance(location[entry], dict)):
             location[entry] = nest(location[entry])
+
+    location["questSettings:10"]["betterquesting:10"]["editmode:1"] = 0
+
     return location
 
 
