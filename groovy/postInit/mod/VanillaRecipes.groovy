8aec76675888eb7a1df4bc02e87dfe9151ce33ba
mtbo <111296252+loxoDev@users.noreply.github.com>
2024-04-26 12:12:51 +0200

    Master quests (#851)
    * Merge master into quest branch (#824)
    
    * uranium stoik fixes
    
    * Update UraniumChain.groovy
    
    * fix some stoik
    
    * thallium chain from zinc
    
    * add thallium dust
    
    * finish thallium and cadmium chain
    
    * fixed everything
    
    * remove circuit 0
    
    * buff latex throughput
    
    you get 4 rubber bar/plate per 1 sulfur now
    
    * fix PVB stoik and PVAc recipe for glue
    
    * readd boiling recipe
    
    * restore PVB recipe to normal?
    
    * radiator stuff
    
    * fix
    
    * fix DTBP distillation recipe
    
    * fix br so2 recipe duration
    
    * fix PVB
    
    * Update AirDistillation.groovy
    
    * buh
    
    * update mgf2 soln. volume convention
    
    * start germanium chain
    
    * fix things
    
    * add waelz oxide proc chain
    
    * add germanium/gallium route 2
    
    * finish up germanium
    
    * update argon-adjusted subcooling
    
    * coagulation tank thingy
    
    * allowing OC recipes to accept power unit regarless of the power units' charge
    
    * finish indium
    
    * init commit
    
    * outline selenium chain
    
    * outline tellurium line
    
    * high purity tellurium outline
    
    * outline high purity selenium
    
    * finish selenium & tellurium
    
    * electrolysis overhaul exodus
    
    * reorganization
    
    * move paper chain
    
    * reduce sulfur consumption for latex
    
    * fix Al2(SO4)3 stoik
    
    * outline caesium chain
    
    * fix co2 dupe
    
    * refactor alcohol & carboxylic acid processes, add formic acid chain
    
    * lepidolite line changes; rubidium line outline
    
    * IR config change in preperation
    
    * Update to SuSyCore 0.1.18
    
    * Make track segments craftable
    
    * Modify recipe for tracks and make the tunnel bore craftable
    
    * finish caesium-rubidium
    
    * delete simplerpc
    
    * revert default quests changes
    
    * fix lang
    
    * update black mesa
    
    * turn cadmium sponge into le block, remove useless crude plat sponge
    
    * update susycore (take 2)
    
    * fixes
    
    * https://www.youtube.com/watch?v=VmGX-IVMpo0
    
    fixes a duplicate and tests a thing
    
    * remove latex for vulcanization
    
    * add metaitems for tunnelbore
    
    * add new metaitems to tunnelbore recipe
    
    * Tunnelbore recipes (MTBOs take)
    
    * Refactor internal naming and add localisation
    
    * Small changes to recipes
    
    * Minor capitalisation mistake
    
    * Mods update (#823)
    
    * update mods
    
    * fix ICBM.groovy
    
    * I'm dumb
    
    * I'm dumb v2
    
    * You can never escape Multiblocked.
    
    ---------
    
    Co-authored-by: bruberu <80226372+bruberu@users.noreply.github.com>
    Co-authored-by: trainvoi <voitrainelephant@gmail.com>
    Co-authored-by: planetme <87911459+planetme@users.noreply.github.com>
    Co-authored-by: MCTian-mi <35869948+MCTian-mi@users.noreply.github.com>
    
    * Tunnelbore Quests
    
    * Unfuck Questbook
    
    * Make Questbook FPS Great Again:tm: (#825)
    
    Changes slicing mode to 2 for everything
    
    * quest fixes for 1.12.6 (#840)
    
    * fix error 1,2,4 in issue #837
    
    * fix Titanium loc and PET requirements
    
    * Fix the one missed instance of electromagnetic
    
    ---------
    
    Co-authored-by: mtbo <111296252+loxoDev@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: bruberu <80226372+bruberu@users.noreply.github.com>
    Co-authored-by: trainvoi <voitrainelephant@gmail.com>
    Co-authored-by: planetme <87911459+planetme@users.noreply.github.com>
    Co-authored-by: MCTian-mi <35869948+MCTian-mi@users.noreply.github.com>
    Co-authored-by: The Science Demon <81928868+TheScienceDemon@users.noreply.github.com>

--- groovy/postInit/mod/VanillaRecipes.groovy
+++ groovy/postInit/mod/VanillaRecipes.groovy
@@ -82,17 +82,7 @@ def recipesToRemove = [
 	'minecraft:andesite',
 	'minecraft:minecart',
 	'quark:trapdoor',
-	'minecraft:golden_apple',
-	'chisel:uncraft_blockcopper',
-	'chisel:uncraft_blocksteel',
-	'chisel:uncraft_blocktin',
-	'chisel:uncraft_blockbronze',
-	'chisel:redstone',
-	'chisel:emerald',
-	'chisel:coal',
-	'chisel:charcoal',
-	'quark:charcoal_block'
-
+	'minecraft:golden_apple'
 ]
 
 for(name in recipesToRemove) {
