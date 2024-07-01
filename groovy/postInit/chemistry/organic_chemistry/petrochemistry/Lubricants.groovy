cd84ec55a152eaf49dd63be27458cb380f7e3085
trainvoi <voitrainelephant@gmail.com>
2024-07-02 00:54:55 +0700

    remove toluene from nitroaniline recipe

--- groovy/postInit/chemistry/organic_chemistry/petrochemistry/Lubricants.groovy
+++ groovy/postInit/chemistry/organic_chemistry/petrochemistry/Lubricants.groovy
@@ -901,7 +901,6 @@ PHASE_SEPARATOR = recipemap('phase_separator')
 
     BR.recipeBuilder()
         .fluidInputs(fluid('two_chloronitrobenzene') * 144)
-        .fluidInputs(fluid('toluene') * 1000)
         .fluidInputs(fluid('ammonia') * 2000)
         .outputs(metaitem('dustAmmoniumChloride') * 6)
         .fluidOutputs(fluid('two_nitroaniline_solution') * 1000)
