e194e8a3e941679cfb592214ac913796ca2bcbb5
planetme <87911459+planetme@users.noreply.github.com>
2024-07-21 09:23:14 -0500

    add crude to main atmo fractions

--- groovy/postInit/chemistry/organic_chemistry/petrochemistry/Petrochemistry.groovy
+++ groovy/postInit/chemistry/organic_chemistry/petrochemistry/Petrochemistry.groovy
@@ -121,10 +121,10 @@ class Petrochemistry = {
     }
 
     public static fractions = [
-        lubricating_oil : new OilFraction('lubricating_oil').withTraits(Sulfuric)
-        heavy_fuel_oil : new OilFraction('heavy_fuel_oil').withTraits(CatalyticCrackable, Sulfuric, Heatable).tap { upgrade_name = 'light_fuel_oil' },
-        light_fuel_oil : new OilFraction('light_fuel_oil').withTraits(CatalyticCrackable, Sulfuric, Heatable).tap { upgrade_name = 'kerosene' },
-        kerosene : new OilFractionCrackable('kerosene').withTraits(CatalyticCrackable, Sulfuric, Heatable).tap { upgrade_name = 'naphtha' },
+        lubricating_oil : new OilFraction('lubricating_oil').withTraits(Sulfuric, Crude)
+        heavy_fuel_oil : new OilFraction('heavy_fuel_oil').withTraits(CatalyticCrackable, Sulfuric, Heatable, Crude).tap { upgrade_name = 'light_fuel_oil' },
+        light_fuel_oil : new OilFraction('light_fuel_oil').withTraits(CatalyticCrackable, Sulfuric, Heatable, Crude).tap { upgrade_name = 'kerosene' },
+        kerosene : new OilFractionCrackable('kerosene').withTraits(CatalyticCrackable, Sulfuric, Heatable, Crude).tap { upgrade_name = 'naphtha' },
         naphtha : new OilFractionCrackable('naphtha').withTraits(Crude),
         light_naphtha : new OilFractionCrackable('light_naphtha').withTraits(Sulfuric, Heatable),
         heavy_naphtha : new OilFractionCrackable('heavy_naphtha').withTraits(Sulfuric, Heatable),
