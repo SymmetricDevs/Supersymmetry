09a732b42f4bc7688b5d9738132e64a83b25ceef
planetme <87911459+planetme@users.noreply.github.com>
2024-07-31 23:44:36 -0500

    outline visbreaking

--- groovy/postInit/chemistry/organic_chemistry/petrochemistry/oil_refining/ThermalCracking.groovy
+++ groovy/postInit/chemistry/organic_chemistry/petrochemistry/oil_refining/ThermalCracking.groovy
@@ -19,7 +19,7 @@ COKING = recipemap('coking_tower')
             .fluidOutputs(fluid('cracked_heavy_gas_oil'))
             .fluidOutputs(fluid('crude_light_gas_oil'))
             .fluidOutputs(fluid('crude_naphtha'))
-            .duration(400)
+            .duration(200)
             .EUt(30)
             .buildAndRegister()
 
@@ -38,11 +38,52 @@ COKING = recipemap('coking_tower')
             .fluidOutputs(fluid('cracked_heavy_gas_oil'))
             .fluidOutputs(fluid('crude_light_gas_oil'))
             .fluidOutputs(fluid('crude_naphtha'))
-            .duration(400)
+            .fluidOutputs(fluid('sulfuric_refinery_gas'))
+            .duration(200)
             .EUt(30)
             .buildAndRegister()
 */
 
-// Visbreaking
+/* Visbreaking
+    // Single Stage
+        CRACKER.recipeBuilder()
+            .fluidInputs(fluid('vacuum_oil_residue') * 1000)
+            .fluidOutputs(fluid('thermally_cracked_vacuum_oil_residue') * 1000)
+            .duration(200)
+            .EUt(Globals.voltAmps[3] * 2)
+            .buildAndRegister()
+        
+        MIXER.recipeBuilder()
+            .fluidInputs(fluid('thermally_cracked_vacuum_oil_residue') * 1000)
+            .fluidInputs(fluid('vacuum_oil_residue') * 1000)
+            .fluidOutputs(fluid('quenched_vacuum_oil_residue') * 2000)
+            .duration(40)
+            .EUt(30)
+            .buildAndRegister()
+
+        DT.recipeBuilder()
+            .fluidInputs(fluid('quenched_vacuum_oil_residue') * 1000)
+            .fluidOutputs(fluid('thermal_cracking_residue'))
+            .fluidOutputs(fluid('cracked_heavy_gas_oil'))
+            .fluidOutputs(fluid('crude_light_gas_oil'))
+            .fluidOutputs(fluid('crude_naphtha'))
+            .fluidOutputs(fluid('sulfuric_refinery_gas'))
+            .duration(200)
+            .EUt(30)
+            .buildAndRegister()
+
+    // Two Stage
+        DT.recipeBuilder()
+            .fluidInputs(fluid('quenched_vacuum_oil_residue') * 1000)
+            .fluidInputs(fluid('doubly_cracked_heavy_gas_oil') * 1000)
+            .fluidOutputs(fluid('thermal_cracking_residue'))
+            .fluidOutputs(fluid('cracked_heavy_gas_oil'))
+            .fluidOutputs(fluid('crude_light_gas_oil'))
+            .fluidOutputs(fluid('crude_naphtha'))
+            .fluidOutputs(fluid('sulfuric_refinery_gas'))
+            .duration(400)
+            .EUt(30)
+            .buildAndRegister()
+*/
 
 // Coking
