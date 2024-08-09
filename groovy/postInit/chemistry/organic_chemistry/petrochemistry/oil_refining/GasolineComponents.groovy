5381161fc18824a6b842c8b8195da66e92b1a1a8
planetme <87911459+planetme@users.noreply.github.com>
2024-08-09 13:39:38 -0500

    reforming overhaul

--- groovy/postInit/chemistry/organic_chemistry/petrochemistry/oil_refining/GasolineComponents.groovy
+++ groovy/postInit/chemistry/organic_chemistry/petrochemistry/oil_refining/GasolineComponents.groovy
@@ -2,6 +2,8 @@ import petrochemistry.Petrochemistry
 
 REACTION_FURNACE = recipemap('reaction_furnace')
 FBR = recipemap('fixed_bed_reactor')
+REFORMER = recipemap('catalytic_reformer_recipes')
+PHASE_SEPARATOR = recipemap('phase_separator')
 
 /* Isomerization
     REACTION_FURNACE.recipeBuilder()
@@ -25,5 +27,40 @@ FBR = recipemap('fixed_bed_reactor')
         .fluidOutputs(fluid('isomerate'))
         .fluidOutputs(fluid('hydrogen'))
         .duration(100)
+        .buildAndRegister()
+*/
+
+/* Alkylation
+
+*/
+
+/* Dimerization
+
+*/
+
+/* Reforming
+    REFORMER.recipeBuilder()
+        .fluidInputs(fluid('heavy_naphtha') * 1000)
+        .fluidInputs(fluid('hydrogen') * 1000)
+        .notConsumable(ore('catalystBedSupportedPlatinum'))
+        .fluidOutputs(fluid('naphtha_reformate') * 4000)
+        .duration(25)
+        .EUt(Globals.voltAmps[3])
+        .buildAndRegister()
+
+    PHASE_SEPARATOR.recipeBuilder()
+        .fluidInputs(fluid('naphtha_reformate') * 4000)
+        .fluidOutputs(fluid('reforming_liquids') * 1000)
+        .fluidOutputs(fluid('reforming_effluent') * 3000)
+        .duration(25)
+        .EUt(Globals.voltAmps[1])
+        .buildAndRegister()
+
+    DT.recipeBuilder()
+        .fluidInputs(fluid('reforming_liquids') * 1000)
+        .fluidOutputs(fluid('reformate') * 600)
+        .fluidOutputs(fluid('lpg') * 200)
+        .fluidOutputs(fluid('refinery_gas') * 200)
+        .duration(400)
         .buildAndRegister()
 */
