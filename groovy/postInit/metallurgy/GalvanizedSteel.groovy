8d5b90ff10c2e004f354a9416f9fe3909a354bf5
trainvoi <voitrainelephant@gmail.com>
2024-09-12 22:14:47 +0700

    Update GalvanizedSteel.groovy

--- groovy/postInit/metallurgy/GalvanizedSteel.groovy
+++ groovy/postInit/metallurgy/GalvanizedSteel.groovy
@@ -9,9 +9,9 @@ import static gregtech.api.unification.ore.OrePrefix.dye;
 FLUID_SOLIDIFIER = recipemap('fluid_solidifier')
 
 FLUID_SOLIDIFIER.recipeBuilder()
-    .inputs(ore('wireFineSteel') * 64)
-    .fluidInputs(fluid('zinc') * 144)
-    .outputs(metaitem('wireFineGalvanizedSteel') * 64)
-    .duration(800)
+    .inputs(ore('wireFineSteel') * 8)
+    .fluidInputs(fluid('zinc') * 18)
+    .outputs(metaitem('wireFineGalvanizedSteel') * 8)
+    .duration(100)
     .EUt(Globals.voltAmps[1])
     .buildAndRegister()
