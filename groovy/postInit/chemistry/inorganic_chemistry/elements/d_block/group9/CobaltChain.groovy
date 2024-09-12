eab9f6ae0ea2055acf60c6711353fb53c181b74e
MCTian-mi <35869948+MCTian-mi@users.noreply.github.com>
2024-09-12 23:56:47 +0800

    Cobalt matte primitive smelting

--- groovy/postInit/chemistry/inorganic_chemistry/elements/d_block/group9/CobaltChain.groovy
+++ groovy/postInit/chemistry/inorganic_chemistry/elements/d_block/group9/CobaltChain.groovy
@@ -2,6 +2,7 @@ import globals.Globals
 import static globals.CarbonGlobals.*
 
 ROASTER = recipemap('roaster')
+PBF_RECIPES = recipemap("primitive_blast_furnace")
 
 for (combustible in combustibles()) {
     ROASTER.recipeBuilder()
@@ -12,4 +13,23 @@ for (combustible in combustibles()) {
         .duration(120)
         .EUt(Globals.voltAmps[2])
         .buildAndRegister()
+}
+
+// Primitive ways for getting impure cobalt (cobalt matte) for Kovar.
+def cobalt_matte_recipes = [
+        ["oreCobaltite", 1],
+        ["oreNetherrackCobaltite", 2],
+        ["oreEndstoneCobaltite", 2],
+        ["dustCobaltite", 1],
+]
+
+cobalt_matte_recipes.forEach { recipe ->
+    combustibles().forEach { combustible ->
+        PBF_RECIPES.recipeBuilder()
+                .inputs(ore(recipe[0]))
+                .inputs(ore(combustible.name) * (combustible.equivalent(2)))
+                .outputs(metaitem('ingotCobaltMatte') * recipe[1])
+                .duration(80 * recipe[1])
+                .buildAndRegister()
+    }
 }
