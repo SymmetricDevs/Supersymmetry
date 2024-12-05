df6004748699c50efd8785137b26d8d3caccbc9c
trexxet <atomnik07@gmail.com>
2024-12-05 23:01:20 +0500

    Battery hulls recipes

--- groovy/postInit/components/Batteries.groovy
+++ groovy/postInit/components/Batteries.groovy
@@ -1,5 +1,6 @@
 import globals.Globals
 import globals.GroovyUtils
+import globals.RecyclingHelper
 
 import gregtech.api.recipes.recipeproperties.RecipeProperty;
 import gregtech.api.recipes.RecipeBuilder;
@@ -309,7 +310,146 @@ EXTRUDER.recipeBuilder()
         .EUt(120)
         .buildAndRegister()
 
-// Hulls
+/*
+ * Hulls
+*/
+
+for (voltage in ['lv', 'mv', 'hv', 'ev', 'iv']) {
+    crafting.remove('gregtech:battery_hull_' + voltage)
+    RecyclingHelper.removeRecyclingRecipes(metaitem('battery.hull.' + voltage))
+}
+
+// LV Battery Hull
+mods.gregtech.assembler.removeByInput(1, [metaitem('cableGtSingleTin'), metaitem('plateBatteryAlloy')], [fluid('plastic') * 144 * 144])
+mods.gregtech.extractor.removeByInput(2, [metaitem('battery.re.lv.cadmium')], null)
+mods.gregtech.extractor.removeByInput(2, [metaitem('battery.re.lv.lithium')], null)
+mods.gregtech.extractor.removeByInput(2, [metaitem('battery.re.lv.sodium')], null)
+
+ASSEMBLER.recipeBuilder()
+        .circuitMeta(5)
+        .inputs(ore('plateStainlessSteel') * 2)
+        .inputs(metaitem('cableGtSingleTin'))
+        .fluidInputs(fluid('plastic') * 144)
+        .outputs(metaitem('battery.hull.lv'))
+        .duration(80)
+        .EUt(120)
+        .buildAndRegister()
+RecyclingHelper.handleRecycling(metaitem('battery.hull.lv'),
+        [ore('plateStainlessSteel') * 2, metaitem('cableGtSingleTin'), ore('dustPlastic')])
+
+// MV Battery Hull
+mods.gregtech.assembler.removeByInput(2, [metaitem('cableGtSingleCopper') * 2, metaitem('plateBatteryAlloy') * 3], [fluid('plastic') * 432 * 432])
+mods.gregtech.assembler.removeByInput(2, [metaitem('cableGtSingleAnnealedCopper') * 2, metaitem('plateBatteryAlloy') * 3], [fluid('plastic') * 432 * 432])
+mods.gregtech.extractor.removeByInput(2, [metaitem('battery.re.mv.cadmium')], null)
+mods.gregtech.extractor.removeByInput(2, [metaitem('battery.re.mv.lithium')], null)
+mods.gregtech.extractor.removeByInput(2, [metaitem('battery.re.mv.sodium')], null)
+
+ASSEMBLER.recipeBuilder()
+        .circuitMeta(5)
+        .inputs(ore('plateStainlessSteel') * 4)
+        .inputs(metaitem('cableGtSingleCopper') * 2)
+        .fluidInputs(fluid('plastic') * 432)
+        .outputs(metaitem('battery.hull.mv'))
+        .duration(160)
+        .EUt(120)
+        .buildAndRegister()
+ASSEMBLER.recipeBuilder()
+        .circuitMeta(5)
+        .inputs(ore('plateStainlessSteel') * 4)
+        .inputs(metaitem('cableGtSingleAnnealedCopper') * 2)
+        .fluidInputs(fluid('plastic') * 432)
+        .outputs(metaitem('battery.hull.mv'))
+        .duration(160)
+        .EUt(120)
+        .buildAndRegister()
+RecyclingHelper.handleRecycling(metaitem('battery.hull.mv'),
+        [ore('plateStainlessSteel') * 4, metaitem('cableGtSingleCopper') * 2, ore('dustPlastic') * 3])
+
+// HV Battery Hull
+mods.gregtech.assembler.removeByInput(4, [metaitem('cableGtSingleGold') * 4, metaitem('plateBatteryAlloy') * 9], [fluid('plastic') * 1296 * 1296])
+mods.gregtech.extractor.removeByInput(2, [metaitem('battery.re.hv.cadmium')], null)
+mods.gregtech.extractor.removeByInput(2, [metaitem('battery.re.hv.lithium')], null)
+mods.gregtech.extractor.removeByInput(2, [metaitem('battery.re.hv.sodium')], null)
+
+ASSEMBLER.recipeBuilder()
+        .circuitMeta(5)
+        .inputs(ore('plateStainlessSteel') * 8)
+        .inputs(metaitem('cableGtSingleGold') * 4)
+        .fluidInputs(fluid('plastic') * 1296)
+        .outputs(metaitem('battery.hull.hv'))
+        .duration(240)
+        .EUt(120)
+        .buildAndRegister()
+RecyclingHelper.handleRecycling(metaitem('battery.hull.hv'),
+        [ore('plateStainlessSteel') * 8, metaitem('cableGtSingleGold') * 4, ore('dustPlastic') * 9])
+
+// EV Battery Hull
+mods.gregtech.assembler.removeByInput(480, [metaitem('cableGtSingleAluminium') * 2, metaitem('plateBlueSteel') * 2], [fluid('polytetrafluoroethylene') * 144 * 144])
+mods.gregtech.extractor.removeByInput(2, [metaitem('battery.ev.vanadium')], null)
+
+ASSEMBLER.recipeBuilder()
+        .circuitMeta(5)
+        .inputs(ore('plateStainlessSteel') * 16)
+        .inputs(metaitem('cableGtSingleAluminium') * 2)
+        .fluidInputs(fluid('polytetrafluoroethylene') * 144)
+        .outputs(metaitem('battery.hull.ev'))
+        .duration(240)
+        .EUt(480)
+        .buildAndRegister()
+RecyclingHelper.handleRecycling(metaitem('battery.hull.ev'),
+        [ore('plateStainlessSteel') * 16, metaitem('cableGtSingleAluminium') * 2, ore('dustPolytetrafluoroethylene')])
+
+// IV Battery Hull
+mods.gregtech.assembler.removeByInput(1920, [metaitem('cableGtSinglePlatinum') * 2, metaitem('plateRoseGold') * 6], [fluid('polytetrafluoroethylene') * 288 * 288])
+mods.gregtech.extractor.removeByInput(2, [metaitem('battery.iv.vanadium')], null)
+
+ASSEMBLER.recipeBuilder()
+        .circuitMeta(5)
+        .inputs(ore('plateStainlessSteel') * 32)
+        .inputs(metaitem('cableGtSinglePlatinum') * 2)
+        .fluidInputs(fluid('polytetrafluoroethylene') * 432)
+        .outputs(metaitem('battery.hull.iv'))
+        .duration(240)
+        .EUt(1920)
+        .buildAndRegister()
+RecyclingHelper.handleRecycling(metaitem('battery.hull.iv'),
+        [ore('plateStainlessSteel') * 32, metaitem('cableGtSinglePlatinum') * 2, ore('dustPolytetrafluoroethylene') * 3])
+
+// Primitive LV Battery Hull
+
+crafting.addShaped('primitive_battery_hull_lv', metaitem('battery.primitivehull.lv'), [
+    [null, metaitem('cableGtSingleTin'), null],
+    [null, ore('plateBatteryAlloy'),     null],
+    [null, ore('plateBatteryAlloy'),     null]
+])
+ASSEMBLER.recipeBuilder()
+        .inputs(ore('plateBatteryAlloy'))
+        .inputs(metaitem('cableGtSingleTin'))
+        .fluidInputs(fluid('plastic') * 144)
+        .outputs(metaitem('battery.primitivehull.lv'))
+        .duration(80)
+        .EUt(30)
+        .buildAndRegister()
+RecyclingHelper.handleRecycling(metaitem('battery.primitivehull.lv'),
+        [ore('plateBatteryAlloy'), metaitem('cableGtSingleTin')])
+
+// Primitive MV Battery Hull
+
+crafting.addShaped('primitive_battery_hull_mv', metaitem('battery.primitivehull.mv'), [
+    [metaitem('cableGtSingleCopper'), null,                     metaitem('cableGtSingleCopper')],
+    [ore('plateBatteryAlloy'),        ore('plateBatteryAlloy'), ore('plateBatteryAlloy')],
+    [ore('plateBatteryAlloy'),        ore('plateBatteryAlloy'), ore('plateBatteryAlloy')]
+])
+ASSEMBLER.recipeBuilder()
+        .inputs(ore('plateBatteryAlloy') * 3)
+        .inputs(metaitem('cableGtSingleCopper') * 2)
+        .fluidInputs(fluid('plastic') * 432)
+        .outputs(metaitem('battery.primitivehull.mv'))
+        .duration(160)
+        .EUt(30)
+        .buildAndRegister()
+RecyclingHelper.handleRecycling(metaitem('battery.primitivehull.mv'),
+        [ore('plateBatteryAlloy') * 3, metaitem('cableGtSingleCopper') * 2])
 
 
 // Remove Batteries
