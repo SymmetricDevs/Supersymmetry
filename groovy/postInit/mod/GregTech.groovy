aeb53f4ef84badbfcfd9b9ba15f2eb667261a8de
trainvoi <voitrainelephant@gmail.com>
2024-12-06 20:00:19 +0700

    add crushing/forge hammer recipes

--- groovy/postInit/mod/GregTech.groovy
+++ groovy/postInit/mod/GregTech.groovy
@@ -763,6 +763,34 @@ mods.gregtech.macerator.recipeBuilder()
 		.EUt(7)
 		.buildAndRegister();
 
+mods.gregtech.forge_hammer.recipeBuilder()
+		.inputs(item('susy:resource_block_1', 1))
+		.outputs(metaitem('gemAnthracite') * 8)
+		.duration(10)
+		.EUt(16)
+		.buildAndRegister();
+
+mods.gregtech.macerator.recipeBuilder()
+		.inputs(item('susy:resource_block_1', 1))
+		.outputs(metaitem('dustAnthracite') * 8)
+		.duration(240)
+		.EUt(7)
+		.buildAndRegister();
+
+mods.gregtech.forge_hammer.recipeBuilder()
+		.inputs(item('susy:resource_block_1', 2))
+		.outputs(metaitem('gemLignite') * 8)
+		.duration(10)
+		.EUt(16)
+		.buildAndRegister();
+
+mods.gregtech.macerator.recipeBuilder()
+		.inputs(item('susy:resource_block_1', 2))
+		.outputs(metaitem('dustgemAnthracite') * 8)
+		.duration(240)
+		.EUt(7)
+		.buildAndRegister();
+
 mods.gregtech.macerator.recipeBuilder()
 		.inputs(metaitem('native_copper'))
 		.outputs(metaitem('dustCopper'))
