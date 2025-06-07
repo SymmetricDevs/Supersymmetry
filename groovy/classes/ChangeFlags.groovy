b3040080d39e40f83a15340a7e21a3262d3aa4b1
WideMann <sokoleoko35@gmail.com>
2025-06-07 20:21:51 +0200

    Adding ores
    Bertrandite and Chromitite

--- groovy/classes/ChangeFlags.groovy
+++ groovy/classes/ChangeFlags.groovy
@@ -730,6 +730,12 @@ class ChangeFlags {
         oreProp = Carnotite.getProperty(PropertyKey.ORE);
         oreProp.setOreByProducts(LimestoneTailings, LimestoneTailings, LimestoneTailings, LimestoneTailings);
 
+        oreProp = Bertrandite.getProperty(PropertyKey.ORE);
+        oreProp.setOreByProducts(PegmatiteTailings, PegmatiteTailings, PegmatiteTailings, PegmatiteTailings);
+
+        oreProp = Chromitite.getProperty(PropertyKey.ORE);
+        oreProp.setOreByProducts(PegmatiteTailings, PegmatiteTailings, PegmatiteTailings, PegmatiteTailings);
+
         oreProp = Cerussite.getProperty(PropertyKey.ORE);
         oreProp.setDirectSmeltResult(Lead);
 
