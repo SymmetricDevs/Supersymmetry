115e2b102dfdc315551b336393aaf2b918b82384
Rick <157808400+Rick8484@users.noreply.github.com>
2025-11-02 16:31:20 +0100

    Add molten Alu Recipe to be free from stupid excractor

--- groovy/postInit/metallurgy/AluminiumAlloys.groovy
+++ groovy/postInit/metallurgy/AluminiumAlloys.groovy
@@ -1,9 +1,16 @@
 import static prePostInit.Recipemaps.*
 import static gregtech.api.GTValues.*
 
+INDUCTION_FURNACE.recipeBuilder()
+        .inputs(metaitem('dustAluminium'))
+        .fluidOutputs(fluid('molten.aluminium') * 144)
+        .EUt(VA[MV])
+        .duration(10)
+        .buildAndRegister()
+
 // 97.6% Al 1.1% Mg 0.64% Si 0.36% Cu 0.30% Cr
 INDUCTION_FURNACE.recipeBuilder()
-        .fluidInputs(fluid('aluminium') * 10144)
+        .fluidInputs(fluid('molten.aluminium') * 10144)
         .inputs(ore('dustTinyMagnesium') * 8)
         .inputs(ore('dustTinySilicon') * 4)
         .inputs(ore('dustTinyCopper') * 1)
@@ -15,7 +22,7 @@ INDUCTION_FURNACE.recipeBuilder()
 
 // 90.6% Al 5.5% Zn 2.4% Mg 1.3% Cu 0.26% Cr
 INDUCTION_FURNACE.recipeBuilder()
-        .fluidInputs(fluid('aluminium') * 10848)
+        .fluidInputs(fluid('molten.aluminium') * 10848)
         .inputs(ore('dustTinyZinc') * 17)
         .inputs(ore('dustTinyMagnesium') * 20)
         .inputs(ore('dustTinyCopper') * 4)
