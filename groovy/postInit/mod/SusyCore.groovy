4d0329ffbe6790054d4312dbbca86d90ad8727fe
Rick <157808400+Rick8484@users.noreply.github.com>
2025-11-03 13:53:53 +0100

    Fix susy block recipes

--- groovy/postInit/mod/SusyCore.groovy
+++ groovy/postInit/mod/SusyCore.groovy
@@ -353,19 +353,12 @@ ASSEMBLER.recipeBuilder()
     .EUt(VA[EV])
     .buildAndRegister()
 
-//Industrial Concrete
-SOLIDIFIER.recipeBuilder()
-    .inputs(ore('frameGtSteel'))
-    .fluidInputs(fluid('concrete') * 576)
-    .outputs(item('susy:susy_stone_smooth', 9) * 32)
-    .duration(100)
-    .EUt(VA[LV])
-    .buildAndRegister()
-
 // Concrete Dust * 1 (remove Industrial Concrete recycling)
 mods.gregtech.macerator.removeByInput(2, [item('susy:susy_stone_smooth', 9)], null)
 // Concrete Dust * 1
 mods.gregtech.macerator.removeByInput(2, [item('susy:susy_stone_cobble', 9)], null)
+
+//Custom Susy Blocks for Nether Complex structure
 
 // Asbestos Fiber
 EXTRUDER.recipeBuilder()
@@ -414,20 +407,15 @@ dyes.each { dye, number ->
         .buildAndRegister()
 }
 
-//Custom Susy Blocks for Black Mesa Research Facility
-MIXER.recipeBuilder() 
-    .inputs(metaitem('dustStone')) 
-    .fluidInputs(fluid('concrete') * 144) 
-    .outputs(item('susy:random_concrete') * 8) 
-    .duration(80)
-    .EUt(VA[LV])
-    .buildAndRegister()
+//Custom Susy Blocks for Black Mesa style Research Facility
 
+//Industrial Concrete
 def concretes = [1, 2, 3]
 
 concretes.each { number ->
-    MIXER.recipeBuilder()
+    ASSEMBLER.recipeBuilder()
         .circuitMeta(number)
+        .inputs(metaitem('frameSteel'))
         .inputs(metaitem('dustStone'))
         .fluidInputs(fluid('concrete') * 144)
         .outputs(item('susy:random_concrete', number) * 8)
@@ -436,6 +424,27 @@ concretes.each { number ->
         .buildAndRegister()
 }
 
+ASSEMBLER.recipeBuilder()
+    .circuitMeta(4)
+    .inputs(metaitem('frameSteel')) 
+    .inputs(metaitem('dustStone'))
+    .fluidInputs(fluid('concrete') * 144) 
+    .outputs(item('susy:random_concrete') * 8) 
+    .duration(80)
+    .EUt(VA[LV])
+    .buildAndRegister()
+
+ASSEMBLER.recipeBuilder()
+    .circuitMeta(5)
+    .inputs(metaitem('frameSteel')) 
+    .inputs(metaitem('dustStone'))
+    .fluidInputs(fluid('concrete') * 144) 
+    .outputs(item('susy:susy_stone_smooth', 9) * 8)
+    .duration(80)
+    .EUt(VA[LV])
+    .buildAndRegister()
+
+//Dotted Panels
 def panels = [4, 5, 6, 7]
 
 panels.each { number ->
@@ -449,6 +458,7 @@ panels.each { number ->
         .buildAndRegister()
 }
 
+//Industrial Cinder Bricks
 def cinders = [8, 9, 10, 11, 12, 13]
 
 cinders.each { number ->
@@ -462,6 +472,7 @@ cinders.each { number ->
         .buildAndRegister()
 } 
 
+//Smooth Industrial Concretes
 def smooths = [14, 15]
 
 smooths.each { number ->
