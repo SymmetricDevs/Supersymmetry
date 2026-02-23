b844e297b744fcec051926cda6c0ea41c4c40a5e
Martin <tsdrppy@gmail.com>
2026-02-23 09:18:45 -0500

    large RES

--- groovy/postInit/gameplay/LargeRailroadEngineeringStation.groovy
+++ groovy/postInit/gameplay/LargeRailroadEngineeringStation.groovy
@@ -1,7 +1,41 @@
-import gregtech.api.multiblock.MultiblockControllerBase
+import globals.Globals
+import static gregtech.api.GTValues.*
+import cam72cam.immersiverailroading.IRItems
+import cam72cam.mod.serialization.TagCompound
+import supersymmetry.api.recipes.SuSyRecipeMaps
+import trackapi.lib.Gauges
 
-supersymmetry.gregtech.multiblock.create("large_railroad_engineeringStation")
-    .withPattern([...]) // Define block structure
-    .withRecipeMap(...) // What recipes it uses
-    .withEUt(512) // Energy usage
-    .register()
+Globals.solders.each { key, val ->
+	TagCompound tag1 = new TagCompound();
+
+	tag1.setString("defID", "rolling_stock/transporter_erector/soyuz_transporter.json");
+	tag1.setFloat("gauge", (float) Gauges.STANDARD);
+	
+	cam72cam.mod.item.ItemStack is6 = new cam72cam.mod.item.ItemStack(IRItems.ITEM_ROLLING_STOCK, 1);
+	is6.setTagCompound(tag1);
+
+	// https://en.wikipedia.org/wiki/Transporter_erector
+	// https://positron96.gitlab.io/projects/soyuz-transporter/
+	SuSyRecipeMaps.LARGE_RAILROAD_ENGINEERING_STATION.recipeBuilder()
+		.circuitMeta(6)
+		.inputs(ore('plateHsla980X') * 64) 
+		//.inputs(ore('frameGtHsla980X') * 64) 
+		//.inputs(ore('gearSteel') * 8) 
+		.inputs(metaitem('minecart_wheels.steel') * 8)
+		.inputs(item('susy:metallurgy', 1) * 8) 
+		//.inputs(metaitem('fluid.regulator.hv') * 4) 
+		//.inputs(metaitem('electric.pump.hv') * 4) 
+		//.inputs(metaitem('electric.motor.hv') * 8) 
+		.inputs(ore('stickLongSteel') * 64) 
+		.inputs(metaitem('sensor.ev') * 4) 
+		.inputs(ore('circuitHv') * 8) 
+		.inputs(metaitem('battery_buffer.hv.4')) 
+		.inputs(ore('cableGtQuadrupleCopper') * 32) 
+		.inputs(ore('pipeNormalFluidSteel') * 32)
+		.fluidInputs(fluid(key) * (val * 20)) 
+		.fluidInputs(fluid('hydraulic_fluid') * 5000)
+		.outputs(is6.internal)
+		.EUt(VA[LV]) 
+		.duration(1)
+		.buildAndRegister();
+}
