2cc53c89354f6488be0d5da7bb5b08bf62fc73e6
Engineerboy2998 <immanueltomczuk@gmail.com>
2026-03-08 17:47:34 +0000

    Added frangible nut metaitem

--- groovy/preInit/RegisterMetaItems.groovy
+++ groovy/preInit/RegisterMetaItems.groovy
@@ -459,6 +459,7 @@ eventManager.listen { PostMaterialEvent event ->
         addItem(10421, "fuel_injector")
         addItem(10422, "augmented_spark_igniter")
         addItem(10423, "slapper_detonator")
+        addItem(10424, "frangible_nut")
     }
 
     log.infoMC("Finished adding metaitems")
