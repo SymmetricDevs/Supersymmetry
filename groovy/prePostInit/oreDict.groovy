d85c5c8f6d0cdb20099b040bf3e2b0adb15cae8a
Fiery36 <coolpunnyskeletondunker@gmail.com>
2026-07-07 13:22:03 -0400

    standardized

--- groovy/prePostInit/oreDict.groovy
+++ groovy/prePostInit/oreDict.groovy
@@ -252,12 +252,12 @@ for (type in StoneVariantBlock.StoneType.values()) {
 ore('defaultCobblestone').add(ore('cobblestone'))
 
 for (i in 0..11) {
-    ore('defaultCobblestone').remove(item('susy:susy_stone_cobble', + i))
+    ore('defaultCobblestone').remove(item('susy:susy_stone_cobble', i))
 }
 
 // Stone Dust
 for (i in 27200..27207) {
-    ore('dustStone').add(item('gregtech:meta_dust', 27200))
+    ore('dustStone').add(item('gregtech:meta_dust', i))
 }
 
 ore('dustStone').add(metaitem('dustQuartzite'))
