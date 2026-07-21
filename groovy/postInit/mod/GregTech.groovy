02fd1cd518f35d5aee7653409a622734f60f8189
Fiery36 <coolpunnyskeletondunker@gmail.com>
2026-07-21 19:09:38 -0400

    solving a problem I caused continued

--- groovy/postInit/mod/GregTech.groovy
+++ groovy/postInit/mod/GregTech.groovy
@@ -1001,21 +1001,25 @@ PACKAGER.recipeBuilder()
     .EUt(VA[ULV])
     .buildAndRegister();
 
-PACKAGER.recipeBuilder()
-    .circuitMeta(2)
-    .inputs(metaitem('dustStone'))
-    .outputs(metaitem('dustSmallStone') * 4)
-    .duration(10)
-    .EUt(12)
-    .buildAndRegister();
+ore('dustStone').each { stack ->
+    def meta = stack.metadata
 
-PACKAGER.recipeBuilder()
-    .circuitMeta(1)
-    .inputs(metaitem('dustStone'))
-    .outputs(metaitem('dustTinyStone') * 9)
-    .duration(10)
-    .EUt(12)
-    .buildAndRegister();
+    PACKAGER.recipeBuilder()
+            .circuitMeta(1)
+            .inputs(item('gregtech:meta_dust', meta))
+            .outputs(item('gregtech:meta_dust_tiny', meta) * 9)
+            .duration(10)
+            .EUt(12)
+            .buildAndRegister()
+
+    PACKAGER.recipeBuilder()
+        .circuitMeta(2)
+        .inputs(item('gregtech:meta_dust', meta))
+        .outputs(item('gregtech:meta_dust_small', meta) * 4)
+        .duration(10)
+        .EUt(12)
+        .buildAndRegister()
+}
 
 crafting.replaceShaped("gregtech:small_dust_disassembling_stone", metaitem('dustSmallStone') * 4, [
     [null, metaitem('dustStone'), null],
