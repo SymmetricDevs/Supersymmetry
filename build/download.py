3a2723ff1a121d552c9b44e75097ed82bd8b6c91
unitazer <unitazermirror@gmail.com>
2026-09-18 17:46:34 +0300

    drop requests dependency

--- build/download.py
+++ build/download.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import requests
+import urllib.request
 import subprocess
 import os
 
@@ -21,16 +21,16 @@ def download():
             + forgeVer
             + "-installer.jar"
         )
-        r = requests.get(url)
-        jar.write(r.content)
+        with urllib.request.urlopen(url) as r:
+            jar.write(r.read())
     print("Forge installer Downloaded")
 
     vanilla = basePath + "/minecraft_server.1.12.2.jar"
     if not os.path.isfile(vanilla):
         with open(basePath + "/minecraft_server.1.12.2.jar", "w+b") as jar:
             url = "https://launcher.mojang.com/v1/objects/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar"
-            r = requests.get(url)
-            jar.write(r.content)
+            with urllib.request.urlopen(url) as r:
+                jar.write(r.read())
     print("Vanilla Downloaded")
 
     subprocess.run(["java", "-jar", "forge-installer.jar",
