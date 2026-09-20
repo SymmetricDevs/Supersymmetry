6e6d29351e5848011d8eb906dcd34e8308b62fe1
bruberu <80226372+bruberu@users.noreply.github.com>
2026-09-20 12:27:50 -0500

    waste gypsum: best recipe award

--- groovy/postInit/chemistry/inorganic_chemistry/elements/s_block/group2/CalciumChain.groovy
+++ groovy/postInit/chemistry/inorganic_chemistry/elements/s_block/group2/CalciumChain.groovy
@@ -9,6 +9,16 @@ ROASTER.recipeBuilder()
         .duration(160)
         .buildAndRegister()
 
+// 56CaSO4(H2O)2 + 40H2O -> 56CaSO4 + 152H2O
+// 7, 5, 7, 19
+ROASTER.recipeBuilder()
+        .fluidInputs(fluid('waste_gypsum') * 19000)
+        .outputs(metaitem('dustCalciumSulfate') * 42)
+        .fluidOutputs(fluid('dense_steam') * 19000)
+        .EUt(VA[MV])
+        .duration(1600)
+        .buildAndRegister()
+
 ROASTER.recipeBuilder()
         .circuitMeta(1)
         .inputs(ore('dustCalciumSulfate') * 6)
@@ -27,7 +37,7 @@ VACUUM_DT.recipeBuilder()
 
 ELECTROLYZER.recipeBuilder()
         .notConsumable(metaitem('graphite_electrode'))
-        .notConsumable(metaitem('stickSteel'))
+        .notConsumable(metaiWtem('stickSteel'))
         .fluidInputs(fluid('calcium_chloride') * 432)
         .fluidOutputs(fluid('chlorine') * 2000)
         .outputs(metaitem('dustCalcium'))
