bee338a7d050a42021bea5bf6102dca72ff3a48d
bruberu <80226372+bruberu@users.noreply.github.com>
2026-09-21 21:37:35 -0500

    me when alice

--- groovy/material/SecondDegreeMaterials.groovy
+++ groovy/material/SecondDegreeMaterials.groovy
@@ -9,6 +9,7 @@ import gregtech.api.fluids.store.FluidStorageKeys
 import gregtech.api.unification.material.properties.*
 import gregtech.api.unification.material.properties.BlastProperty.GasTier
 import supersymmetry.api.unification.material.properties.SuSyPropertyKey
+import supersymmetry.api.unification.material.properties.SolidRocketFuelProperty
 
 import supersymmetry.api.util.SuSyUtility
 
@@ -1275,7 +1276,7 @@ class SecondDegreeMaterials {
                 .flags(DISABLE_DECOMPOSITION)
                 .build()
         
-        //ALICE.setProperty(SuSyPropertyKey.SOLID_ROCKET_FUEL, new SolidRocketFuelProperty(0.108, 0.075, 180))
+        ALICE.setProperty(SuSyPropertyKey.SOLID_ROCKET_FUEL, new SolidRocketFuelProperty(0.108, 0.075, 180))
 
     }
 }
